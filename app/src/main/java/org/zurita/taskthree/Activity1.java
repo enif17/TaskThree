7b2e60c2770397d286e6ea458dc1f23be3009282
enif17 <zuritamr17@gmail.com>
2016-02-16 10:09:27 +0100

    The email button was deleted from all layouts. The explicit intent is created. The Activity2 just shows a button to be able to see visually that it works.

--- app/src/main/java/org/zurita/taskthree/Activity1.java
+++ app/src/main/java/org/zurita/taskthree/Activity1.java
@@ -1,5 +1,6 @@
 package org.zurita.taskthree;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,12 +22,20 @@ public class Activity1 extends AppCompatActivity {
 
     }
 
+    //onImplicit function launches when the implicit button is clicked
     public void onImplicit(View view) {
         Log.d("piruli", "Implicit button clicked");
     }
 
+    //onExplicit function launches when the explicit button is clicked
     public void onExplicit(View view) {
         Log.d("piruli", "Explicit button clicked");
+
+        //a new intent is created associated with Activity2
+        Intent explicit = new Intent(this, Activity2.class);
+        //throws the intent; Activity2 is now running
+        startActivity(explicit);
     }
+
 
 }
